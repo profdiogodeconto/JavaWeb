e79d8690f1c69a632895d99b737f8c831994618c
dideconto <digoo.deconto@up.edu.br>
2017-11-14 21:51:55 -0200

    Ecommerce: alterar URL no redirecionamento da página

--- Ecommerce/src/controller/MProdutoBean.java
+++ Ecommerce/src/controller/MProdutoBean.java
@@ -9,9 +9,9 @@ import model.Produto;
 
 @ManagedBean(name = "mProdutoBean")
 public class MProdutoBean {
-	
+
 	private Produto produto = new Produto();
-	private ArrayList<Produto> produtos = new ArrayList<Produto>();	
+	private ArrayList<Produto> produtos = new ArrayList<Produto>();
 
 	public ArrayList<Produto> getProdutos() {
 		return ProdutoDAO.listarProdutos();
@@ -23,40 +23,18 @@ public class MProdutoBean {
 
 	public void setProduto(Produto produto) {
 		this.produto = produto;
-	}	
-
-	//Action
-	public String adicionarProduto(Produto p){
-		ProdutoDAO.cadastrarProduto(p);
-		return "ListarProdutos.xhtml";
 	}
-	
+
+	// Action
+	public String adicionarProduto(Produto p) {
+		ProdutoDAO.cadastrarProduto(p);
+		return "ListarProdutos.xhtml?faces-redirect=true";
+	}
 
 }
 
-//Criar a anotação do ManagedBean
-//Importar do pacote javax.faces.*
-//Criar atributo do modelo
-//Criar os getters and setters do atributo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Criar a anotação do ManagedBean
+// Importar do pacote javax.faces.*
+// Criar atributo do modelo
+// Criar os getters and setters do atributo
+// Criar a Action
