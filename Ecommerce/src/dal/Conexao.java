254123b804e1751dd1e9ed511d3f9ce740b28923
dideconto <digoo.deconto@up.edu.br>
2017-11-21 20:25:20 -0200

    Ecommerce: Configurar o banco de dados e conexão

---
+++ Ecommerce/src/dal/Conexao.java
@@ -0,0 +1,17 @@
+package dal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Conexao {
+	
+	private static final String NOME_DA_CONEXAO = "EcommerceDB";
+	private static final EntityManagerFactory EMF = 
+			Persistence.createEntityManagerFactory(NOME_DA_CONEXAO);
+	
+	public static EntityManager getEntityManager(){
+		return EMF.createEntityManager();
+	}
+
+}
